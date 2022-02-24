ed861d4fc5b297518b7f9d3e11166a28a29c7330
unknown <jankesmateusz@gmail.com>
2022-02-24 19:28:40 +0100

    Revert "lepsze wyswietlanie w konsoli"
    This reverts commit 1ddc2a6930070766eb66101af613fac523617e33.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String... args) throws Exception {
 
-        String date = "2022-02-24";          /*   PROBLEM Z ASC <--- */
+        String date = "2022-02-23";          /*   PROBLEM Z ASC <--- */
 
         String csv_url = "https://ezamowienia.gov.pl/mo-board/api/v1/Board/GetSearchNoticesListReport?noticeType=ContractNotice&isTenderAmountBelowEU=true&" +
                 "publicationDateFrom=" + date + "T00:00:00.000Z&orderType=Delivery&SortingColumnName=PublicationDate&SortingDirection=DESC&reportType=0";
@@ -23,16 +23,8 @@ public class Main {
         List<Tender> tenders = new TenderFactory().getTenderList(list);
         Collections.reverse(tenders);
 
-        int counter = 1;
-        int pageNumber = (int) Math.ceil(list.size()/10.0);
-        System.out.println("-------------------- STRONA " + pageNumber + " --------------------");
         for(Tender t : tenders){
-            counter++;
             System.out.println(t.getTenderInfo());
-            if(counter % 10 == 0 & pageNumber != 0){
-                pageNumber--;
-                System.out.println("-------------------- STRONA " + pageNumber + " --------------------");
-            }
         }
         System.out.println(tenders.size());
     }
