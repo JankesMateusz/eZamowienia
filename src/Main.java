3371ed1cc4635c6af3ba345967ba134215dc9a82
unknown <jankesmateusz@gmail.com>
2022-02-25 09:47:07 +0100

    should work as bash script

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String... args) throws Exception {
 
-        String date = "2022-02-24";          /*   PROBLEM Z ASC <--- */
+        String date = "";          /*   PROBLEM Z ASC <--- */
+//        if(args.length > 0){
+//            date = args[0];
+//        }
+
+        System.out.println("Filter? y/n");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
 
         String csv_url = "https://ezamowienia.gov.pl/mo-board/api/v1/Board/GetSearchNoticesListReport?noticeType=ContractNotice&isTenderAmountBelowEU=true&" +
                 "publicationDateFrom=" + date + "T00:00:00.000Z&orderType=Delivery&SortingColumnName=PublicationDate&SortingDirection=DESC&reportType=0";
@@ -21,13 +29,22 @@ public class Main {
         list = new ListCombiner(list, cpvList).outcome();
 
         List<Tender> tenders = new TenderFactory().getTenderList(list);
-        tenders = new TenderFilter(tenders).filterTenders();
+
+        if(answer.equals("y")){
+
+            tenders = new TenderFilter(tenders).filterTenders();
+            System.out.println(tenders);
+        }
+
         Collections.reverse(tenders);
 
         for(Tender t : tenders){
-            System.out.println(t.getTenderInfo());
+            System.out.println("LP: " + (tenders.indexOf(t) + 1) + "\n" + t.getTenderInfo());
         }
-        System.out.println(tenders.size());
+        System.out.println("TOTAL: " + tenders.size());
+
+
+        scanner.nextLine();
     }
 }
 
